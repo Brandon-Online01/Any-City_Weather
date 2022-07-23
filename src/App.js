28ed485ca7caf844d0f68b5e24a7bb3ad9767152
Brandon Nhlanhla Nkawu <brandon@abangamedia.co.za>
2022-07-23 17:55:52 +0200

    refactored the form and added new icons for other elements, cleared lon and lat functionality, fixed the resposiveness of the whole page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 import './App.css';
@@ -10,11 +10,16 @@ import HumidityIcon from './assets/icons/humidity.png'
 import WindDirection from './assets/icons/windock.png'
 import WindSpeed from './assets/icons/anemometer.png'
 import CloudCover from './assets/icons/clouds.png'
+import Sunset from './assets/icons/sunset.png'
+import Sunrise from './assets/icons/sunrise.png'
 
 import { Container } from './components/styled';
 import { WeatherSection } from './components/styled'
 import { Header } from './components/styled';
 import { FormWrapper } from './components/styled';
+import { Form } from './components/styled'
+import { Input } from './components/styled'
+import { Button } from './components/styled'
 import { InfoBody } from './components/styled';
 import { WeatherElement } from './components/styled';
 import { WeatherHeader } from './components/styled';
@@ -23,19 +28,6 @@ import { WeatherIcon } from './components/styled';
 import { WeatherInfo } from './components/styled';
 
 const App = () => {
-    // const notify = () => toast("Wow so easy !");
-
-    const [lat, setLat] = useState([]);
-    const [long, setLong] = useState([]);
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition ( position => {
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
-        });
-
-    }, []);
-
     const [requestedCity, setRequestedCity] = useState([]);
 
     const handleSubmit = (e) => {
@@ -45,7 +37,6 @@ const App = () => {
         }
         else {
             toast.success('Great, Busy Searching......')
-            console.log(requestedCity)
         }
     }
 
@@ -55,15 +46,18 @@ const App = () => {
                 <WeatherSection>
                     <Header>Any City Weather</Header>
                     <FormWrapper>
-                    <form className='weather-section__form' onSubmit={handleSubmit}>
-                            <input  type='text'
-                                    value={requestedCity} 
-                                    onInput={e => setRequestedCity(e.target.value)} 
-                                    placeholder='Enter any city'/>
-                            <button type='submit' 
-                                    className='weather-section__btn' 
-                                    onClick={handleSubmit}>Check</button>
-                    </form>
+                        <Form>
+                            <Input  type='text' 
+                                    value={requestedCity}
+                                    onSubmit={handleSubmit} 
+                                    placeholder='Any City Name'
+                                    onInput={e => setRequestedCity(e.target.value)}
+                            />
+                            <Button
+                                    type='submit'
+                                    onClick={handleSubmit}
+                            >Check</Button>
+                        </Form>
                     </FormWrapper>
                     <InfoBody>
                         <WeatherElement>
@@ -104,38 +98,17 @@ const App = () => {
                     </InfoBody>
                     <InfoBody>
                         <WeatherElement>
-                            <WeatherHeader>Temperature</WeatherHeader>
+                            <WeatherHeader>Sunrise</WeatherHeader>
                             <WeatherValues>
-                                <WeatherIcon src={tempIcon} alt="Any City Weather" />
-                                <WeatherInfo>°C</WeatherInfo>
+                                <WeatherIcon src={Sunset} alt="Any City Weather" />
+                                <WeatherInfo>am</WeatherInfo>
                             </WeatherValues>
                         </WeatherElement>
                         <WeatherElement>
-                            <WeatherHeader>Humidity</WeatherHeader>
+                            <WeatherHeader>Sunset</WeatherHeader>
                             <WeatherValues>
-                                <WeatherIcon src={HumidityIcon} alt="Any City Weather" />
-                                <WeatherInfo>%</WeatherInfo>
-                            </WeatherValues>
-                        </WeatherElement>
-                        <WeatherElement>
-                            <WeatherHeader>Wind Speed</WeatherHeader>
-                            <WeatherValues>
-                                <WeatherIcon src={WindSpeed} alt="Any City Weather" />
-                                <WeatherInfo>km/hr</WeatherInfo>
-                            </WeatherValues>
-                        </WeatherElement>
-                        <WeatherElement>
-                            <WeatherHeader>Wind Direction</WeatherHeader>
-                            <WeatherValues>
-                                <WeatherIcon src={WindDirection} alt="Any City Weather" />
-                                <WeatherInfo>Direction</WeatherInfo>
-                            </WeatherValues>
-                        </WeatherElement>
-                        <WeatherElement>
-                            <WeatherHeader>Cloud Cover</WeatherHeader>
-                            <WeatherValues>
-                                <WeatherIcon src={CloudCover} alt="Any City Weather" />
-                                <WeatherInfo>%</WeatherInfo>
+                                <WeatherIcon src={Sunrise} alt="Any City Weather" />
+                                <WeatherInfo>pm</WeatherInfo>
                             </WeatherValues>
                         </WeatherElement>
                     </InfoBody>
--- src/components/styled.js
+++ src/components/styled.js
@@ -9,27 +9,75 @@ export const Header = styled.h1`
 `;
 
 export const WeatherSection = styled.div`
-    width: 50%;
+    width: 100%;
     margin: 100px auto;
 `;
 
 export const FormWrapper = styled.div`
-    max-width: 500px;
-    margin: 0 auto;
     display: flex;
 `;
 
+export const Form = styled.div`
+    width: 100%;
+    text-align: center;
+`
+
+export const Input = styled.input`
+    padding: 15px;
+    border: 1px solid #b8b8b8;
+    border-radius: 5px 0 0 5px;
+    transition: 0.2s all ease-in-out;
+    font-size: 16px;
+    max-width: 180px;
+
+    &:hover {
+        border: 1px solid #009425;
+
+        &:focus {
+            outline: none;
+            border: 1px solid #009425;
+
+            &::placeholder {
+                font-style: italic;
+                font-weight: 100;
+            }
+        }
+    }
+`
+
+export const Button = styled.button`
+    background-color: transparent;
+    color: #ffffff;
+    padding: 15px;
+    border: 1px solid #009425;
+    border-radius: 0 5px 5px 0;
+    transition: 0.2s all ease-in-out;
+    font-size: 16px;
+    cursor: pointer;
+    font-weight: 400;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+
+    &:hover {
+        background-color: #009425;
+    }
+`
+
 export const InfoBody = styled.div`
+    gap: 10px;
     display: flex;
+    color: #ffffff;
+    margin: 50px 0;
     flex-wrap: wrap;
+    font-weight: 700;
+    text-align: center;
     align-items: center;
     justify-content: space-evenly;
-    margin: 50px 0 0 0;
-    color: #fff;
-    font-weight: 700;
 `;
 
 export const WeatherElement = styled.div`
+    max-width: 250px;
+    width: 100%;
 `;
 
 export const WeatherHeader = styled.p`
@@ -50,4 +98,4 @@ export const WeatherInfo = styled.p`
 export const WeatherIcon = styled.img`
     width: 30px;
     height: 30px;
-`
+`;
